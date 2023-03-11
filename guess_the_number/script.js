0a6914773590a26839cc0ce2f890da1731588ff1
viktor1798 <vktrsmsk1798@gmail.com>
2023-03-11 23:09:59 +0300

    refactoring code

--- guess_the_number/script.js
+++ guess_the_number/script.js
@@ -6,42 +6,54 @@ let highscore = 0;
 
 console.log(secretNum);
 
+const gameOver = () => {
+  document.querySelector('.guess-message').textContent = 'Игра окончена!';
+  document.querySelector('.score').textContent = `0`;
+  document.querySelector('.question').textContent = `Game Over!!!`;
+  document.querySelector('body').style.backgroundColor = `#ff0000`;
+  document.querySelector('.question').style.width = `55rem`;
+};
+
+const numberGuessed = () => {
+  if (highscore < scoreCount) {
+    highscore = scoreCount;
+    document.querySelector('.highscore').textContent = `${highscore}`;
+  }
+  document.querySelector('.score').textContent = `${scoreCount}`;
+  document.querySelector('.guess-message').textContent = 'Правильно';
+  document.querySelector('.question').textContent = `Congratulations `;
+  document.querySelector('body').style.backgroundColor = `#09fa15`;
+  document.querySelector('.question').style.width = `70rem`;
+};
+
+const score = () => {
+  scoreCount--;
+  document.querySelector('.score').textContent = `${scoreCount}`;
+};
+
 const guessingNumber = () => {
-  const inputNum = Number(document.querySelector('.number-input').value);
+  let inputNum = Number(document.querySelector('.number-input').value);
 
   if (!inputNum || inputNum > 20 || inputNum < 1) {
     document.querySelector('.guess-message').textContent =
       'Введите число от 1 до 20!';
   } else if (inputNum === secretNum) {
-    if (highscore < scoreCount) {
-      document.querySelector('.highscore').textContent = `${scoreCount}`;
-    }
-    document.querySelector('.score').textContent = `${scoreCount}`;
-    document.querySelector('.guess-message').textContent = 'Правильно';
-    document.querySelector('.question').textContent = `Congratulations `;
-    document.querySelector('body').style.backgroundColor = `#09fa15`;
-    document.querySelector('.question').style.width = `70rem`;
+    numberGuessed();
   } else if (inputNum < secretNum) {
     if (scoreCount > 1) {
-      document.querySelector('.question').textContent = `Мало`;
-      scoreCount--;
-      document.querySelector('.score').textContent = `${scoreCount}`;
+      document.querySelector('.question').textContent = `${inputNum}`;
+      document.querySelector('.guess-message').textContent = `Мало`;
+      score();
     } else {
-      document.querySelector('.guess-message').textContent = 'Игра окончена!';
-      document.querySelector('.score').textContent = `0`;
-      document.querySelector('.question').textContent = `Game Over!!!`;
-      document.querySelector('body').style.backgroundColor = `#ff0000`;
-      document.querySelector('.question').style.width = `55rem`;
+      gameOver();
     }
   } else if (inputNum > secretNum) {
     if (scoreCount > 1) {
-      document.querySelector('.question').textContent = `Много`;
-      scoreCount--;
-      document.querySelector('.score').textContent = `${scoreCount}`;
+      document.querySelector('.question').textContent = `${inputNum}`;
+      document.querySelector('.guess-message').textContent = `Много`;
+      score();
     } else {
-      document.querySelector('.guess-message').textContent = 'Игра окончена!';
-      document.querySelector('.score').textContent = `0`;
-      document.querySelector('.question').textContent = `???`;
+      gameOver();
     }
   }
 };
@@ -55,7 +67,6 @@ const restartGame = () => {
   document.querySelector('.question').textContent = `???`;
   document.querySelector('body').style.backgroundColor = `#000000`;
   document.querySelector('.question').style.width = `25rem`;
-//   Number(document.querySelector('.number-input').value) = 1;
 };
 
 document.querySelector('.check').addEventListener('click', guessingNumber);
